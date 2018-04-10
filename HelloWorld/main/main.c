eeb74fa95680cb6f523221e276667cf54b9b6a24
TaraHoleInIt <tara.a.k.86@gmail.com>
2018-04-09 18:35:32 -0700

    Initial commit

---
+++ HelloWorld/main/main.c
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2017-2018 Tara Keeling
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "ssd1306.h"
+#include "ssd1306_draw.h"
+#include "ssd1306_font.h"
+#include "ssd1306_default_if.h"
+
+#define USE_I2C_DISPLAY
+#define USE_SPI_DISPLAY
+
+#if defined USE_I2C_DISPLAY
+    static const int I2CDisplayAddress = 0x3C;
+    static const int I2CDisplayWidth = 128;
+    static const int I2CDisplayHeight = 64;
+    static const int I2CResetPin = -1;
+
+    struct SSD1306_Device I2CDisplay;
+#endif
+
+#if defined USE_SPI_DISPLAY
+    static const int SPIDisplayChipSelect = 15;
+    static const int SPIDisplayWidth = 128;
+    static const int SPIDisplayHeight = 64;
+    static const int SPIResetPin = -1;
+
+    struct SSD1306_Device SPIDisplay;
+#endif
+
+void SetupDemo( struct SSD1306_Device* DisplayHandle, const struct SSD1306_FontDef* Font );
+void SayHello( struct SSD1306_Device* DisplayHandle, const char* HelloText );
+
+bool DefaultBusInit( void ) {
+    #if defined USE_I2C_DISPLAY
+        assert( SSD1306_I2CMasterInitDefault( ) == true );
+        assert( SSD1306_I2CMasterAttachDisplayDefault( &I2CDisplay, I2CDisplayWidth, I2CDisplayHeight, I2CDisplayAddress, I2CResetPin ) == true );
+    #endif
+
+    #if defined USE_SPI_DISPLAY
+        assert( SSD1306_SPIMasterInitDefault( ) == true );
+        assert( SSD1306_SPIMasterAttachDisplayDefault( &SPIDisplay, I2CDisplayWidth, I2CDisplayHeight, SPIDisplayChipSelect, I2CResetPin ) == true );
+    #endif
+
+    return true;
+}
+
+void SetupDemo( struct SSD1306_Device* DisplayHandle, const struct SSD1306_FontDef* Font ) {
+    SSD1306_Clear( DisplayHandle, SSD_COLOR_BLACK );
+    SSD1306_SetFont( DisplayHandle, Font );
+}
+
+void SayHello( struct SSD1306_Device* DisplayHandle, const char* HelloText ) {
+    SSD1306_FontDrawAnchoredString( DisplayHandle, TextAnchor_Center, HelloText, SSD_COLOR_WHITE );
+    SSD1306_Update( DisplayHandle );
+}
+
+void app_main( void ) {
+    printf( "Ready...\n" );
+
+    if ( DefaultBusInit( ) == true ) {
+        printf( "BUS Init lookin good...\n" );
+        printf( "Drawing.\n" );
+
+        #if defined USE_I2C_DISPLAY
+            SetupDemo( &I2CDisplay, &Font_droid_sans_fallback_25x25 );
+            SayHello( &I2CDisplay, "Hello i2c!" );
+        #endif
+
+        #if defined USE_SPI_DISPLAY
+            SetupDemo( &SPIDisplay, &Font_liberation_mono_17x27 );
+            SayHello( &SPIDisplay, "Hi SPI!" );
+        #endif
+
+        printf( "Done!\n" );
+    }
+}
